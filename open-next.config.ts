50424635c85735f541aec20209ef519b4d751151
Malik <ryanhaialafc@gmail.com>
2026-04-21 06:52:02 -0300

    remove wrangler.toml e simplifica open-next config

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,12 +1,7 @@
 import type { OpenNextConfig } from "@opennextjs/cloudflare";
 
 const config: OpenNextConfig = {
-  default: {
-    override: {
-      wrapper: "cloudflare-node",
-      converter: "edge",
-    },
-  },
+  default: {},
 };
 
 export default config;
