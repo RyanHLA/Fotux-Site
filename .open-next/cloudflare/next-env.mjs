9ae53c37192bb6d8ee1b4f99f758d4d5726d6884
Malik <ryanhaialafc@gmail.com>
2026-04-22 02:18:50 -0300

    chore: ignore .open-next build artifacts

--- .open-next/cloudflare/next-env.mjs
+++
@@ -1,3 +0,0 @@
-export const production = {"NEXT_PUBLIC_SUPABASE_URL":"https://ihocawxzfissoxemguba.supabase.co","NEXT_PUBLIC_SUPABASE_ANON_KEY":"sb_publishable_BanS01mzAfTouqmKkB26gA_UgpXJffA","NEXT_PUBLIC_ADMIN_URL":"http://localhost:8081","NEXT_PUBLIC_SITE_URL":"http://localhost:3000"};
-export const development = {"NEXT_PUBLIC_SUPABASE_URL":"https://ihocawxzfissoxemguba.supabase.co","NEXT_PUBLIC_SUPABASE_ANON_KEY":"sb_publishable_BanS01mzAfTouqmKkB26gA_UgpXJffA","NEXT_PUBLIC_ADMIN_URL":"http://localhost:8081","NEXT_PUBLIC_SITE_URL":"http://localhost:3000"};
-export const test = {};
